829e36961c3c3dbfe3e399071871c54f73c0c7d3
Alfiado Constantino <alfiado.ngana@xit2s.com>
2024-07-17 14:36:44 +0200

    use spies

---
+++ src/components/pagination.spec.tsx
@@ -0,0 +1,43 @@
+import { render } from '@testing-library/react'
+import { userEvent } from '@testing-library/user-event'
+
+import { Pagination } from './pagination'
+
+describe('Pagination', () => {
+  it('should display the right amount of pages and results', () => {
+    const wrapper = render(
+      <Pagination
+        pageIndex={0}
+        totalCount={200}
+        perPage={10}
+        // eslint-disable-next-line prettier/prettier
+        onPageChange={() => { }}
+      />,
+    )
+
+    expect(wrapper.getByText('Pagina 1 de 20')).toBeInTheDocument()
+    expect(wrapper.getByText('Total de 200 item(s)')).toBeInTheDocument()
+  })
+
+  const onPageChangeCallback = vi.fn()
+  it('should be able to navigate to the next page', async () => {
+    const wrapper = render(
+      <Pagination
+        pageIndex={0}
+        totalCount={200}
+        perPage={10}
+        onPageChange={onPageChangeCallback}
+      />,
+    )
+
+    const nextPageButton = wrapper.getByRole('button', {
+      name: 'Proxima pagina',
+    })
+
+    const user = userEvent.setup()
+
+    await user.click(nextPageButton)
+
+    expect(onPageChangeCallback).toHaveBeenCalledWith(1)
+  })
+})
