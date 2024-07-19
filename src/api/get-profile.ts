87b508d100841ac7f16e512d1c8a99ee709be540
Alfiado Constantino <alfiado.ngana@xit2s.com>
2024-07-19 05:29:18 +0200

    update profile mock

--- src/api/get-profile.ts
+++ src/api/get-profile.ts
@@ -1,13 +1,13 @@
 import { api } from '@/lib/axios'
 
-interface GetProfileResponseProps {
-  name: string
-  id: string
-  email: string
-  phone: string | null
-  role: 'manager' | 'customer'
-  createdAt: Date | null
-  updatedAt: Date | null
+export interface GetProfileResponseProps {
+  name: string;
+  id: string;
+  email: string;
+  phone: string | null;
+  role: 'manager' | 'customer';
+  createdAt: Date | null;
+  updatedAt: Date | null;
 }
 
 export async function getProfile() {
--- src/api/get-restaurant-manager.ts
+++ src/api/get-restaurant-manager.ts
@@ -1,12 +1,12 @@
 import { api } from '@/lib/axios'
 
 export interface GetRestaurantManagerResponseProps {
-  name: string
-  id: string
-  createdAt: Date | null
-  updatedAt: Date | null
-  description: string | null
-  managerId: string | null
+  name: string;
+  id: string;
+  createdAt: Date | null;
+  updatedAt: Date | null;
+  description: string | null;
+  managerId: string | null;
 }
 
 export async function getRestaurantManager() {
